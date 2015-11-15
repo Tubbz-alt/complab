f0afad60678fa1bdb56887da594e0a4dd276af93
Miquel Torrens <miquel@Miquels-MacBook.local>
2015-11-16 00:27:30 +0100

    configuration file set up

--- project/setup.sh
+++ project/setup.sh
@@ -11,52 +11,52 @@ target_dir=$HOME/Desktop/pub_html
 case $cmd in
 
 install)
-	# Run R scripts
-	echo "Installing... "
-	RScript syntax/00_start.R pwd install force
-	
-	# Run MySQL scripts
-	if [ $pswd=='' ]
-	then
-		/usr/local/mysql/bin/mysql -u $user < syntax/data_structure.sql
-		/usr/local/mysql/bin/mysql -u $user < syntax/results_framework.sql
-	else
-		/usr/local/mysql/bin/mysql -u $user -p$pswd < syntax/data_structure.sql
-		/usr/local/mysql/bin/mysql -u $user -p$pswd < syntax/results_framework.sql
-	fi
-	
-	# Create the App folder
-	mkdir -p "$target_dir/app"
-	cp -rf web/* "$target_dir/app"
-	echo "Done!"
-	;;
+  # Run R scripts
+  echo "Installing... "
+  RScript syntax/00_start.R pwd install force
+  
+  # Run MySQL scripts
+  if [ $pswd=='' ]
+  then
+    /usr/local/mysql/bin/mysql -u $user < syntax/data_structure.sql
+    /usr/local/mysql/bin/mysql -u $user < syntax/results_framework.sql
+  else
+    /usr/local/mysql/bin/mysql -u $user -p$pswd < syntax/data_structure.sql
+    /usr/local/mysql/bin/mysql -u $user -p$pswd < syntax/results_framework.sql
+  fi
+  
+  # Create the App folder
+  mkdir -p "$target_dir/app"
+  cp -rf web/* "$target_dir/app"
+  echo "Done!"
+  ;;
 
 uninstall)
-	# Erase database from mysql
-	echo "Uninstalling... "
-	if [ $pswd=='' ]
-	then
-		/usr/local/mysql/bin/mysql -u $user -e "DROP DATABASE omsong;" 
-	else
-		/usr/local/mysql/bin/mysql -u $user -p$pswd -e "DROP DATABASE omsong;" 
-	fi
+  # Erase database from mysql
+  echo "Uninstalling... "
+  if [ $pswd=='' ]
+  then
+    /usr/local/mysql/bin/mysql -u $user -e "DROP DATABASE omsong;" 
+  else
+    /usr/local/mysql/bin/mysql -u $user -p$pswd -e "DROP DATABASE omsong;" 
+  fi
 
-	# Erase thins on App
-	rm -rf "target_dir/app"
-	echo "Done!"
-	;;
+  # Erase thins on App
+  rm -rf "target_dir/app"
+  echo "Done!"
+  ;;
 
 run)
-	# Run R scripts
-	echo "Running..."
-	RScript /syntax/00_start.R pwd analysis
-	RScript /syntax/00_start.R pwd run
+  # Run R scripts
+  echo "Running..."
+  RScript /syntax/00_start.R pwd analysis
+  RScript /syntax/00_start.R pwd run
 
-	# Web plots
-	cp web/categories_network.png "$target_dir/app"
-	;;
+  # Web plots
+  cp web/categories_network.png "$target_dir/app"
+  ;;
 
 *)
-	echo "Unknown Command!"
+  echo "Unknown Command!"
 
 esac
