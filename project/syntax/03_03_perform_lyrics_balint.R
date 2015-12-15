30fd661dfdabfe110f191bc3c20d09534fcea808
vanbalint <vanbalint@gmail.com>
2015-12-15 16:25:40 +0100

    comment fix

--- project/syntax/03_03_perform_lyrics_balint.R
+++ project/syntax/03_03_perform_lyrics_balint.R
@@ -22,9 +22,12 @@ sample.lyrics <- sample.lyrics[!(sample.lyrics$word %in% stopwords("italian")),]
 all <- ddply(sample.lyrics, "word", summarise, freq=sum(word_count) )
 ord <- order(all$freq)
 all <- all[order(all$freq),]
+
+#making wordcloud
 png("wordcloud.png", width=800,height=800)
 wordcloud(all$word, all$freq,  max.words=150, scale=c(8,1), colors=brewer.pal(8,"Dark2"))
 dev.off()
+# #big version for background
 # png("wordcloud_big.png", width=3000,height=3000)
 # wordcloud(all$word, all$freq, scale=c(16,2), max.words=500, colors=rgb(0.192, 0.545,1 ,0.25))
 # dev.off()
