c8fc2a818910a5918542ddffbc098df298522e15
vanbalint <vanbalint@gmail.com>
2015-12-15 16:14:12 +0100

    lyrics stuff

---
+++ project/syntax/03_03_perform_lyrics_balint.R
@@ -0,0 +1,30 @@
+library(RColorBrewer)
+library(wordcloud)
+library(plyr)
+library(tidyr)
+
+
+load("lyrics_p.RData")
+load("song_metadata.RData")
+
+
+#joining tables
+sample.lyrics$word <- words[sample.lyrics$word_index]
+
+#removing stop words
+sample.lyrics <- sample.lyrics[!(sample.lyrics$word %in% stopwords()),]
+sample.lyrics <- sample.lyrics[!(sample.lyrics$word %in% stopwords("german")),]
+sample.lyrics <- sample.lyrics[!(sample.lyrics$word %in% stopwords("french")),]
+sample.lyrics <- sample.lyrics[!(sample.lyrics$word %in% stopwords("spanish")),]
+sample.lyrics <- sample.lyrics[!(sample.lyrics$word %in% stopwords("italian")),]
+
+#frequency list for every (remaining) word)
+all <- ddply(sample.lyrics, "word", summarise, freq=sum(word_count) )
+ord <- order(all$freq)
+all <- all[order(all$freq),]
+png("wordcloud.png", width=800,height=800)
+wordcloud(all$word, all$freq,  max.words=150, scale=c(8,1), colors=brewer.pal(8,"Dark2"))
+dev.off()
+# png("wordcloud_big.png", width=3000,height=3000)
+# wordcloud(all$word, all$freq, scale=c(16,2), max.words=500, colors=rgb(0.192, 0.545,1 ,0.25))
+# dev.off()
